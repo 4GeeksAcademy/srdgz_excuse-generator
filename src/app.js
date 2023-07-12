8bec9b6299c9a93e0ce2d7fcc43e6484aad6619a
Sandra Rodríguez <133523307+srdgz@users.noreply.github.com>
2023-07-12 15:57:12 +0000

    Primera subida

--- src/app.js
+++ src/app.js
@@ -1,11 +1,30 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector("#excusa").innerHTML = generador();
+  console.log("");
+};
+let generador = function() {
+  let pronombre = ["el", "un"];
+  let sujeto = ["policía", "zorro", "perro", "conductor", "cómico", "ladrón"];
+  let verbo = ["cogió mi", "quitó mi", "robó mi", "se llevó mi"];
+  let objeto = ["coche", "zapato", "ordenador", "bolso"];
+  let donde = ["en mi casa", "en la calle", "en el parque", "en el súper"];
+
+  let pronombreIndex = Math.floor(Math.random() * pronombre.length);
+  let sujetoIndex = Math.floor(Math.random() * sujeto.length);
+  let verboIndex = Math.floor(Math.random() * verbo.length);
+  let objetoIndex = Math.floor(Math.random() * objeto.length);
+  let dondeIndex = Math.floor(Math.random() * donde.length);
+
+  return (
+    pronombre[pronombreIndex] +
+    " " +
+    sujeto[sujetoIndex] +
+    " " +
+    verbo[verboIndex] +
+    " " +
+    objeto[objetoIndex] +
+    " " +
+    donde[dondeIndex]
+  );
 };
